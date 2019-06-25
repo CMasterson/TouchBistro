238f9d4c7cade1ac1d320a22b35a07dd3663d07c
Conor Masterson <conor@clearbridgemobile.com>
2019-06-25 19:39:54 -0400

    Initial commit

---
+++ BillTotalCalculator/BillTotalCalculator/BillTotalCalculator.h
@@ -0,0 +1,19 @@
+//
+//  BillTotalCalculator.h
+//  BillTotalCalculator
+//
+//  Created by Conor Masterson on 2019-06-25.
+//  Copyright © 2019 Conor Masterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BillTotalCalculator.
+FOUNDATION_EXPORT double BillTotalCalculatorVersionNumber;
+
+//! Project version string for BillTotalCalculator.
+FOUNDATION_EXPORT const unsigned char BillTotalCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BillTotalCalculator/PublicHeader.h>
+
+
